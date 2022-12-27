e4c9de6d18e55597fba744c7cab061a2dca3506d
Levi <84652664+levirenato@users.noreply.github.com>
2022-12-27 09:14:30 -0300

    Update app.py

--- Dashboard_Carteira/app.py
+++ Dashboard_Carteira/app.py
@@ -7,18 +7,18 @@ base = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQwXe7ngD2zN
 df_meta = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQwXe7ngD2zNlEJdVw62Oejsb7UbeVlVHgClpceNjZKvpCji3gX8KszbQdgZUKHhCWi5IA2aihV8t_y/pub?gid=874097056&single=true&output=csv", sep=",")
 #Prpduto
 fig_produto = px.histogram(base,x = base["PRODUTO"], y=base["FATURAVEL"], text_auto=True)
-fig_produto.update_layout(font=dict(size=20),margin={"r":0,"t":0,"l":0,"b":0})
+fig_produto.update_layout(font=dict(size=18),margin={"r":0,"t":0,"l":0,"b":0})
 # Cliente
 fig_cliente = px.histogram(base,x = base["CLIENTES"], y=[base["FATURAVEL"],base["VAZAMENTO"]], text_auto=True)
-fig_cliente.update_layout(margin={"r":0,"t":0,"l":0,"b":0},legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01),xaxis_range=["YPLASTIC", "LIMPIDA"])  
+fig_cliente.update_layout(font=dict(size=18),margin={"r":0,"t":0,"l":0,"b":0},legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01),xaxis_range=["YPLASTIC", "LIMPIDA"])  
 # MEta
 df_injecao = df_meta.query("SETOR == 'INJECAO'")
 df_sopro = df_meta.query("SETOR == 'SOPRO'")
  # INJECAO
-fig_meta_injecao = px.bar(df_injecao, x="PRODUTO",y=[df_injecao["VALOR ( AC)"],df_injecao["META (R$)"]], text_auto=True)
+fig_meta_injecao = px.bar(df_injecao, x="PRODUTO",y=[df_injecao["VALOR ( AC)"],df_injecao["META (R$)"]], text_auto=True,font=dict(size=18))
  # SOPRO
 fig_meta_sopro = px.bar(df_sopro, x="PRODUTO",y=[df_sopro["VALOR ( AC)"],df_sopro["META (R$)"]], text_auto=True)
-fig_meta_sopro.update_layout(legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01))  
+fig_meta_sopro.update_layout(font=dict(size=18),legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01))  
 
 #Layout
 with st.container():
